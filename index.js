676f265e1036fc915f819df612f418fc6a454136
serapath (Alexander Praetorius) <dev@serapath.de>
2015-06-09 00:42:03 +0200

    ADD babelify transform

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 'use strict';
 /******************************************************************************
   DEPENDENCIES = CUSTOM SDK [Custom Software Development Kit]
