75138458cee84fcc07e64796cf5233544b6f6d13
serapath (Alexander Praetorius) <dev@serapath.de>
2016-02-20 01:57:40 +0100

    UPDATE exemethod dependency

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 /******************************************************************************
   DEPENDENCIES = CUSTOM SDK [Custom Software Development Kit]
 ******************************************************************************/
-var jsonmatter    = require('json-matter');
-var marked        = require('marked');
-var html2markdown = require('html2markdown');
-var method        = require('exemethod')(function(a,b){return b;})
-var fs            = require('fs');
-var os            = require('os');
+var jsonmatter    = require('json-matter')
+var marked        = require('marked')
+var html2markdown = require('html2markdown')
+var method        = require('exemethod')(function(a,b){return b})
+var fs            = require('fs')
+var os            = require('os')
 /******************************************************************************
   PARAMETER = ARGUMENT + [Sanitize & Validate]
 ******************************************************************************/
-var args          = process.argv.slice(2) || [];
+var args          = process.argv.slice(2)
 function setInput (error, mode, string, filename) {
-  if (error) { throw error; }
-  var $mode       = mode;
-  var $string     = string;
-  var $filename   = filename;
+  if (error) { throw error }
+  var $mode       = mode
+  var $string     = string
+  var $filename   = filename
 }
 /******************************************************************************
   EXPORT
@@ -33,20 +33,20 @@ if ({'required':true,'browserify':true}[method]) {
   if (process.stdin.isTTY) {
     // SERVER $> cli --server
     if (args[0] === '--server') {
-      console.log('SERVER with REPL');
-      console.log('To abort press: CTRL+D or CTRL+C');
-      setInput(null, args[0], null, null);
-      startDeamon();
+      console.log('SERVER with REPL')
+      console.log('To abort press: CTRL+D or CTRL+C')
+      setInput(null, args[0], null, null)
+      startDeamon()
     // CLI $> cli --serialize filename
     } else {
-      console.log('NORMAL CLI EXECUTION');
-      throw new Error('@TODO: serialize/parse from cli not implemented yet!');
+      console.log('NORMAL CLI EXECUTION')
+      throw new Error('@TODO: serialize/parse from cli not implemented yet!')
       if (args[1]) { // CLI + 2 args
-        setInput(null, args[0], null, args[1]);
+        setInput(null, args[0], null, args[1])
       } else if (args[0]) { // CLI + 1 arg
-        setInput(null, '--parse', null, args[0]);
+        setInput(null, '--parse', null, args[0])
       } else { // CLI + no args
-        setInput(new Error('@TODO: add --help option & show when given no args'));
+        setInput(new Error('@TODO: add --help option & show when given no args'))
       }
     }
   // $ echo 'foo' | node -p -e 'process.stdin.isTTY' // => undefined
@@ -55,36 +55,36 @@ if ({'required':true,'browserify':true}[method]) {
     setInput(new Error('@TODO: add stream into server deamon process'));
   // PIPED
   } else if (args[0]) {
-    var $mode = args[0];
-    startStream();
-    startDeamon();
+    var $mode = args[0]
+    startStream()
+    startDeamon()
   } else if (!args.length) {
-    var $mode     = '--parse';
-    startStream();
-    startDeamon();
+    var $mode     = '--parse'
+    startStream()
+    startDeamon()
   } else {
-    throw new Error('@TODO: whats wrong here???');
+    throw new Error('@TODO: whats wrong here???')
   }
 } else {
-  throw new Error('@TODO: unsupported method: '+method);
+  throw new Error('@TODO: unsupported method: '+method)
 }
 /******************************************************************************
   PIPE STREAM
 ******************************************************************************/
 function startStream () {
-  process.stdin.setEncoding('utf8');
+  process.stdin.setEncoding('utf8')
   process.stdin.on('data', function(data) {
     return {
       "--parse"     : function (string) {
-        var result = parse(string);
-        process.stdout.write(JSON.stringify(result, null, 2));
+        var result = parse(string)
+        process.stdout.write(JSON.stringify(result, null, 2))
       },
       "--serialize" : function (string) {
-        var result = serialize(JSON.parse(string));
-        process.stdout.write(result);
+        var result = serialize(JSON.parse(string))
+        process.stdout.write(result)
       }
-    }[$mode](data);
-  });
+    }[$mode](data)
+  })
 }
 /******************************************************************************
   UNIX SIGNALS
@@ -92,7 +92,7 @@ function startStream () {
 function startDeamon () {
   // ps aux | grep yourscript
   // kill -s SIGINT [process_id]
-  process.stdin.resume();
+  process.stdin.resume()
   process.on('SIGINT', function (err) {
     // An easy way to send the SIGINT signal is with Control-C in most terminal programs.
     // Note:
@@ -107,11 +107,11 @@ function startDeamon () {
     //   SIGKILL cannot have a listener installed, it will unconditionally terminate node on all platforms.
     //   SIGSTOP cannot have a listener installed.
     //   Note that Windows does not support sending Signals, but node offers some emulation with process.kill(), and child_process.kill(): - Sending signal 0 can be used to search for the existence of a process - Sending SIGINT, SIGTERM, and SIGKILL cause the unconditional exit of the target process.
-    console.log('Got a SIGINT. Goodbye cruel world.');
+    console.log('Got a SIGINT. Goodbye cruel world.')
     if (err) {
-      process.exit(1);
+      process.exit(1)
     } else {
-      process.exit(0);
+      process.exit(0)
     }
   });
 }
@@ -121,20 +121,20 @@ function startDeamon () {
 function parse (string) {
   var result = jsonmatter.parse(string, {
     regex: /^[\s\t\n\r]*---{1}[\s\t\n\r]*(\{[\s\S]*\}[\s\t\n\r]*)(---{1})/
-  });
-  var markdown        = result.__content__;
-  var html            = marked(markdown).replace(/\r?\n|\r/g, "");
-  result.__content__  = patch(html, result.custom);
-  return result;
+  })
+  var markdown        = result.__content__
+  var html            = marked(markdown).replace(/\r?\n|\r/g, "")
+  result.__content__  = patch(html, result.custom)
+  return result
 }
 function serialize (object) {
-  var html    = object.__content__;
-  html        = unpatch(html, object.custom);
-  object.__content__  = html2markdown(html);
+  var html    = object.__content__
+  html        = unpatch(html, object.custom)
+  object.__content__  = html2markdown(html)
   var result  = jsonmatter.serialize(object, {
     delimiter: os.EOL+'---'+os.EOL+os.EOL
-  });
-  return '---' + os.EOL + result;
+  })
+  return '---' + os.EOL + result
 }
 function unpatch (html, custom) {
   // for (var key in custom) {
@@ -154,13 +154,13 @@ function unpatch (html, custom) {
 }
 function patch (html, custom) {
   for (var key in custom) {
-    var regx = new RegExp('<a href="{{(' + key + ')}}">[^<>]*<\/a>', 'g');
+    var regx = new RegExp('<a href="{{(' + key + ')}}">[^<>]*<\/a>', 'g')
     html = html.replace(regx, function (match, contents, offset, s) {
-      return custom[key];
+      return custom[key]
     })
-    var regx = new RegExp('{{' + key + '}}', 'g');
+    var regx = new RegExp('{{' + key + '}}', 'g')
     html = html.replace(regx, function (match, contents, offset, s) {
-      return custom[key];
+      return custom[key]
     })
   }
   return html
